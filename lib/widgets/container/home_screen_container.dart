cbcc820cd59ece78d81bbfb79e43d4786ed5678e
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2024-05-08 10:48:55 +0300

    feat: add HomeScreenContainer widget

---
+++ lib/widgets/container/home_screen_container.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:soccer_info/widgets/date_picker.dart';
+import 'package:soccer_info/widgets/days_scroll.dart';
+import 'package:soccer_info/widgets/leagues_dropdown.dart';
+
+class HomeScreenContainer extends StatelessWidget {
+  final Widget child;
+  final String leagueValue;
+
+  final List<String> leagueNames;
+  final void Function(String? newValue) selectLeagueHandler;
+
+  final DateTime shownDay;
+
+  final void Function(DateTime date) onPickDayHandler;
+  final void Function(String date) onTapDayHandler;
+
+  const HomeScreenContainer({
+    super.key,
+    required this.child,
+    required this.leagueValue,
+    required this.leagueNames,
+    required this.selectLeagueHandler,
+    required this.shownDay,
+    required this.onPickDayHandler,
+    required this.onTapDayHandler,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+      margin: const EdgeInsets.symmetric(vertical: 32, horizontal: 12),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                "المباريات",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 120,
+                    child: LeaguesDropdownButton(
+                      leagueValue: leagueValue,
+                      leagueNames: leagueNames,
+                      selectLeagueHandler: selectLeagueHandler,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 12,
+                  ),
+                  DatePicker(
+                    shownDay: shownDay,
+                    onPickDayHandler: onPickDayHandler,
+                  )
+                ],
+              )
+            ],
+          ),
+          Container(
+            width: double.infinity,
+            height: 60,
+            decoration: BoxDecoration(
+              border: Border(
+                bottom: BorderSide(color: Colors.grey.shade300),
+              ),
+            ),
+            child: DaysScroll(
+              shownDay: shownDay,
+              onTapDayHandler: onTapDayHandler,
+            ),
+          ),
+          Expanded(
+            child: child,
+          ),
+        ],
+      ),
+    );
+  }
+}
